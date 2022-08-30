bb9891e3f1a6393cc72b185fa3162160938a9732
Muzahid1 <vakaliyamuzahid31@gmail.com>
2022-08-30 11:55:11 +0530

    new2

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+       scrollFunction()
+};
+
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "15px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
-    document.getElementById("navbar").style.backgroundColor = "#fff";
-  } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
-    document.getElementById("navbar").style.backgroundColor = "#f8f9fa";
-  }
+       var btn = document.getElementById("home")
+       if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+              btn.style.display = "block"
+              document.getElementById("navbar").style.padding = "15px 10px";
+              document.getElementById("logo").style.fontSize = "25px";
+              document.getElementById("navbar").style.backgroundColor = "#fff";
+       } else {
+              document.getElementById("navbar").style.padding = "80px 10px";
+              document.getElementById("logo").style.fontSize = "35px";
+              document.getElementById("navbar").style.backgroundColor = "#f8f9fa";
+              btn.style.display = "none"
+       }
 }
 
-function nav () {
-   console.log("jd");
-  let nav =  document.getElementById("navbar-right");
-  if (nav.style.display == "none") {
-    nav.style.display = "block"
-    
-  }
-  else {
-    nav.style.display = "none"
-  }
+function nav() {
+       console.log("jd");
+       let nav = document.getElementById("navbar-right");
+       if (nav.style.display == "none") {
+              nav.style.display = "block"
+
+       }
+       else {
+              nav.style.display = "none"
+       }
 }
+
+
 
 // fade in 
